08b09635a19a2a34d46ae7cf479a92a4d8ad7486
Mika Kaakinen <mika.kaakinen@gmail.com>
2026-02-27 09:39:12 +0200

    App.css ja App.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -298,6 +298,7 @@ function GetTeamInfo({ id }) {
                         textAlign: 'center',
                         color: '#fff',
                         backgroundColor: '#EF4444',
+                        padding: '0.75rem 0',
                       }}
                     >
                       {' '}
@@ -308,6 +309,7 @@ function GetTeamInfo({ id }) {
                         textAlign: 'center',
                         color: '#fff',
                         backgroundColor: '#3B82F6',
+                        padding: '0.75rem 0',
                       }}
                     >
                       {' '}
